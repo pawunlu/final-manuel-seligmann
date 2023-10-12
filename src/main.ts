c99ff7cc28e42ac6da5b0ad2373cb69e9f3f4517
manu-seligmann <manuseligmann@hotmail.com>
2023-10-12 18:33:33 -0300

    General improvements

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,25 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import * as hbs from 'hbs';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'templates', 'pages'));
   hbs.registerPartials(join(__dirname, '..', 'templates'));
   app.setViewEngine('hbs');
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`App listening in port ${port}`);
+  });
 }
 bootstrap();
